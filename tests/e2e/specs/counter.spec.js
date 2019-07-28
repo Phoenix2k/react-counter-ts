6b1456dd1f19c7a791a9399b04f87f6f0740a28f
Niko Pettersen <niko.pettersen@gmail.com>
2019-07-29 02:23:01 +0300

    Add Cypress + end to end tests

---
+++ tests/e2e/specs/counter.spec.js
@@ -0,0 +1,21 @@
+describe("Basic counting", () => {
+  it("Root url responds", () => {
+    cy.visit("/");
+    cy.get("header h1").contains("React");
+    cy.get("header h1").contains("Counter");
+  });
+  it("Counts up and down but never goes below zero", () => {
+    cy.visit("/");
+    cy.get(".counter-value").should("have.value", "0");
+    cy.get('[data-action="increment"]').click();
+    cy.get(".counter-value").should("have.value", "1");
+    cy.get('[data-action="increment"]').click();
+    cy.get(".counter-value").should("have.value", "2");
+    cy.get('[data-action="decrement"]').click();
+    cy.get(".counter-value").should("have.value", "1");
+    cy.get('[data-action="decrement"]').click();
+    cy.get(".counter-value").should("have.value", "0");
+    cy.get('[data-action="decrement"]').click();
+    cy.get(".counter-value").should("have.value", "0");
+  });
+});
